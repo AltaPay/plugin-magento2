195838739639289da83d519791834a9f4ccb451f
sz-altapay-com <sz@altapay.com>
2018-10-23 14:54:59 +0200

    Set the correct order status and state before the payment, according to the merchant choice

--- Observer/BeforePaymentObserver.php
+++ Observer/BeforePaymentObserver.php
@@ -3,10 +3,20 @@ namespace SDM\Altapay\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Sales\Model\Order;
 use SDM\Altapay\Model\SystemConfig;
 
 class BeforePaymentObserver implements ObserverInterface
 {
+	/**
+	 * @var SystemConfig
+	 */
+	protected $systemConfig;
+
+	public function __construct(SystemConfig $systemConfig)
+	{
+		$this->systemConfig = $systemConfig;
+	}
 
     /**
      * @param Observer $observer
@@ -14,14 +24,26 @@ class BeforePaymentObserver implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-
+	    $orderState = Order::STATE_NEW;
+	    $orderStatus = Order::STATE_NEW;
         $payment = $observer['payment'];
         if (in_array($payment->getMethod(), SystemConfig::getTerminalCodes())) {
+	        /**
+	         * @var \Magento\Sales\Model\Order
+	         */
             $order = $payment->getOrder();
-            // Dont send any mails until payment is complete
-            $order->setCanSendNewEmailFlag(false);
+	        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+	        $storeCode = $order->getStore()->getCode();
+
+	        //Set the first order state and status (custom, if applicable)
+	        $customFirstOrderStatus = $this->systemConfig->getStatusConfig('before', $storeScope, $storeCode);
+	        if ($customFirstOrderStatus) {
+		        $orderStatus = $customFirstOrderStatus;
+	        }
+	        $order->setState($orderState)->setStatus($orderStatus);
+	        // Do not send any mails until payment is complete
+	        $order->setCanSendNewEmailFlag(false);
+	        $order->getResource()->save($order);
         }
-
     }
-
 }
