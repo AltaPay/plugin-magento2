e05d5eba4e9bf2de5b3aef4ce1ce7fcd58e5c905
sz-altapay-com <sz@altapay.com>
2018-11-21 09:42:57 +0100

    Cancel the payment if the consumer goes away from the payment form with the back button Mark this action in the admin, in the order details

--- Observer/CheckoutCartIndex.php
+++ Observer/CheckoutCartIndex.php
@@ -3,7 +3,6 @@ namespace SDM\Altapay\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use SDM\Altapay\Model\SystemConfig;
 
 class CheckoutCartIndex implements ObserverInterface
 {
@@ -13,19 +12,22 @@ class CheckoutCartIndex implements ObserverInterface
 
     /** @var \Magento\Quote\Model\QuoteFactory */
     private $quoteFactory;
-    protected $_messageManager;
 
+    /** @var \Magento\Framework\Message\ManagerInterface */
+    protected $messageManager;
+     
     /**
      * Constructor
      *
      * @param \Magento\Framework\App\Action\Context $context
      * @param \Magento\Checkout\Model\Session $session
+     * @param \Magento\Framework\Message\ManagerInterface $messageManager
      * @param \Magento\Quote\Model\QuoteFactory $quoteFactory
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\Message\ManagerInterface $messageManager,
         \Magento\Checkout\Model\Session $session,
+        \Magento\Framework\Message\ManagerInterface $messageManager,
         \Magento\Quote\Model\QuoteFactory $quoteFactory
     ) {
         $this->session = $session;
@@ -44,9 +46,19 @@ class CheckoutCartIndex implements ObserverInterface
         $order = $this->session->getLastRealOrder();
         $quote = $this->quoteFactory->create()->loadByIdWithoutStore($order->getQuoteId());
         if ($quote->getId()) {
+            //get quote Id from order and set as active
             $quote->setIsActive(1)->setReservedOrderId(null)->save();
             $this->session->replaceQuote($quote)->unsLastRealOrderId();
-            $this->messageManager->addErrorMessage('Payment Failed.');
+
+            //set order status and comments
+            $historyComment = 'Payment failed! Consumer has pressed the back button from the payment page.';
+
+            $order->setState(\Magento\Sales\Model\Order::STATE_CANCELED);
+            $order->setIsNotified(false);
+            $order->addStatusHistoryComment($historyComment, \Magento\Sales\Model\Order::STATE_CANCELED);
+            $order->getResource()->save($order);
+            //show fail message
+            $this->messageManager->addErrorMessage('Payment failed due to the browser back button usage!');
         }
     }
 }
