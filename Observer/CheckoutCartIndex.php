f53632c921d96bde016d1037634b4e1ca0e785fe
sz-altapay-com <sz@altapay.com>
2018-12-05 19:24:49 +0100

    Fix the bug with the order-cleanup script

--- Observer/CheckoutCartIndex.php
+++ Observer/CheckoutCartIndex.php
@@ -3,6 +3,8 @@ namespace SDM\Altapay\Observer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Sales\Model\OrderFactory;
+use Magento\Framework\Exception\LocalizedException;
 
 class CheckoutCartIndex implements ObserverInterface
 {
@@ -15,6 +17,9 @@ class CheckoutCartIndex implements ObserverInterface
 
     /** @var \Magento\Framework\Message\ManagerInterface */
     protected $messageManager;
+
+    /** @var \Magento\Sales\Model\OrderFactory */
+    protected $orderFactory;
      
     /**
      * Constructor
@@ -23,16 +28,19 @@ class CheckoutCartIndex implements ObserverInterface
      * @param \Magento\Checkout\Model\Session $session
      * @param \Magento\Framework\Message\ManagerInterface $messageManager
      * @param \Magento\Quote\Model\QuoteFactory $quoteFactory
+     * @param \Magento\Sales\Model\OrderFactory $orderFactory
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Checkout\Model\Session $session,
         \Magento\Framework\Message\ManagerInterface $messageManager,
-        \Magento\Quote\Model\QuoteFactory $quoteFactory
+        \Magento\Quote\Model\QuoteFactory $quoteFactory,
+        \Magento\Sales\Model\OrderFactory $orderFactory
     ) {
         $this->session = $session;
         $this->quoteFactory = $quoteFactory;
         $this->messageManager = $messageManager;
+        $this->orderFactory   = $orderFactory;
     }
 
 
@@ -43,22 +51,34 @@ class CheckoutCartIndex implements ObserverInterface
      */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $order = $this->session->getLastRealOrder();
-        $quote = $this->quoteFactory->create()->loadByIdWithoutStore($order->getQuoteId());
-        if ($quote->getId()) {
-            //get quote Id from order and set as active
-            $quote->setIsActive(1)->setReservedOrderId(null)->save();
-            $this->session->replaceQuote($quote)->unsLastRealOrderId();
+        if ($this->session->getLastRealOrderId()) {
+            try {
+                $orderId = $this->session->getLastRealOrderId();
+                $order = $orderId ? $this->orderFactory->create()->load($orderId) : false;
+                if ($order->getAltapayPaymentFormUrl()) {
+                    $quote = $this->quoteFactory->create()->loadByIdWithoutStore($order->getQuoteId());
+                    //get quote Id from order and set as active
+                    $quote->setIsActive(1)->setReservedOrderId(null)->save();
+                    $this->session->replaceQuote($quote)->unsLastRealOrderId();
 
-            //set order status and comments
-            $historyComment = 'Payment failed! Consumer has pressed the back button from the payment page.';
+                     
+                    $historyComment = 'Payment failed! Consumer has pressed the back button from the payment page.';
 
-            $order->setState(\Magento\Sales\Model\Order::STATE_CANCELED);
-            $order->setIsNotified(false);
-            $order->addStatusHistoryComment($historyComment, \Magento\Sales\Model\Order::STATE_CANCELED);
-            $order->getResource()->save($order);
-            //show fail message
-            $this->messageManager->addErrorMessage('Payment failed due to the browser back button usage!');
+                    $order->setState(\Magento\Sales\Model\Order::STATE_CANCELED);
+                    $order->setIsNotified(false);
+                    $order->addStatusHistoryComment($historyComment, \Magento\Sales\Model\Order::STATE_CANCELED);
+                    $order->getResource()->save($order);
+
+                    $this->messageManager->addErrorMessage('Payment failed due to the browser back button usage!');
+                }
+            } catch (LocalizedException $e) {
+                // catch and continue - do something when needed
+            } catch (\Exception $e) {
+                // catch and continue - do something when needed
+            }
+
         }
     }
 }
+
+
