e148282cebd5475ffd5acbb5bcdb697cc74dde58
Muhammad Idrees <saadidrees57@gmail.com>
2021-05-07 15:00:45 +0500

    Commented lines are removed and Readme content moved to main Readme file

---
+++ Test/Integration Test/Readme.md
@@ -0,0 +1,14 @@
+# How to run cypress test successfully in your environment 
+
+## Prerequisites: 
+
+1) Magento2 should be installed on publically accessible URL
+2) Cypress should be installed
+
+## Steps 
+
+1) Install dependencies `npm i`
+
+2) Update "cypress/fixtures/config.json" 
+
+3) Execute `./node_modules/.bin/cypress run` in the terminal to run all the tests
---
+++ Test/Integration Test/cypress/integration/examples/payment.js
@@ -0,0 +1,44 @@
+import Order from '../PageObjects/objects'
+
+
+
+
+describe ('Magento2', function(){
+
+    it('CC Payment', function(){
+
+        const ord = new Order()
+        ord.clrcookies()
+        ord.visit()
+        ord.signin()
+        ord.addproduct()
+        ord.cc_payment()
+        ord.admin()
+        ord.capture()
+    })
+
+    it('Klarna Payment', function(){
+
+        const ord = new Order()
+        ord.clrcookies()
+        ord.visit()
+        ord.signin()
+        ord.addproduct()
+        ord.klarna_payment()
+        ord.admin()
+        ord.capture()
+    })
+
+    it('Subscription', function(){
+
+        const ord = new Order()
+        ord.clrcookies()
+        ord.visit()
+        ord.signin()
+        ord.subscription_product()
+        ord.subscription_payment()
+        ord.admin()
+        ord.capture_subscription()
+    })
+
+})
